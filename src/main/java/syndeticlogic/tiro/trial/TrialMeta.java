aee3c1e8f0d61e030e3786631b5ba3ff3a4f82dc
James Percent <james@syndeticlogic.org>
2013-03-01 13:28:02 -0500

    Whoops - forgot to commit this file with the last 1.  Also removed dead code.

--- src/main/java/syndeticlogic/tiro/trial/TrialMeta.java
+++ src/main/java/syndeticlogic/tiro/trial/TrialMeta.java
@@ -1,12 +1,21 @@
 package syndeticlogic.tiro.trial;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
 public class TrialMeta {
     private long id;
     private final String name;
     
-    public TrialMeta(String name) {
+    public TrialMeta(long id, String name) {
         this.name = name;
-        id = -1;
+        this.id = id;
+    }    
+    
+    public TrialMeta(String name) {
+        this(-1, name);
     }
     
     public long getId() {
@@ -21,8 +30,38 @@ public class TrialMeta {
         return name;
     }
 
-    public TrialMeta(long id, String name) {
-        this(name);
-        this.id = id;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TrialMeta other = (TrialMeta) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    public static class TrialMetaRowMapper implements RowMapper<TrialMeta> {
+        @Override
+        public TrialMeta mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new TrialMeta(rs.getLong("id"), rs.getString("name"));
+        }
+    }   
 }
