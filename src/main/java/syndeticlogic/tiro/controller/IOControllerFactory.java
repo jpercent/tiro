caf4d480e066802961c0b0f024ce43db3308c54f
James Percent <james@syndeticlogic.org>
2013-03-04 16:09:39 -0500

    Added IOControllerFactory - a place holder for IOController creation

---
+++ src/main/java/syndeticlogic/tiro/controller/IOControllerFactory.java
@@ -0,0 +1,9 @@
+package syndeticlogic.tiro.controller;
+
+import syndeticlogic.tiro.persistence.Controller;
+
+public class IOControllerFactory {
+    public IOController createIOController(Controller model) {
+        return null;
+    }
+}
