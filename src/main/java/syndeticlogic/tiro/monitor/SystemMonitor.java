ce4625e7a4fcdd72037fb4eab981d5fc4c223963
James Percent <james@syndeticlogic.org>
2013-03-04 13:22:31 -0500

    Added a factory method to SystemMonitor, as well as some contracts.

--- src/main/java/syndeticlogic/tiro/monitor/SystemMonitor.java
+++ src/main/java/syndeticlogic/tiro/monitor/SystemMonitor.java
@@ -9,24 +9,46 @@ import syndeticlogic.tiro.persistence.MemoryStats;
 public class SystemMonitor extends AbstractMonitor implements MemoryMonitor, IOMonitor {
 	private MemoryMonitor memoryMonitor;
 	private IOMonitor ioMonitor;
+	private enum State { Created, Started, Finished };
+	private State state = State.Created;
+	
+	public static SystemMonitor createSystemMonitor(Monitor.Platform platform, String device) {
+	    final SystemMonitor monitor;
+	    switch(platform) {
+	    case OSX:
+	        monitor = new SystemMonitor(new OSXMemoryMonitor(), new OSXIOMonitor(device));
+	        break;
+	    case Linux:
+	    case Windows:
+	    default:
+	        throw new RuntimeException("unsupported platform");
+	    }
+	    return monitor;
+	}
 	
 	public SystemMonitor(MemoryMonitor memoryMonitor, IOMonitor ioMonitor) {
 	    super();
 	    this.memoryMonitor = memoryMonitor;
 	    this.ioMonitor = ioMonitor;
 	}
+	
     @Override
 	public void start() {
+        assert state == State.Created;
 	    memoryMonitor.start();
 	    ioMonitor.start();
+	    state = State.Started;
 	}
     @Override    	
 	public void finish() {
+        assert state == State.Started;
 	    memoryMonitor.finish();
 	    ioMonitor.finish();
+	    state = State.Finished;
 	}
     @Override   
     public void dumpData() {
+        assert state == State.Finished;
         memoryMonitor.dumpData();
         ioMonitor.dumpData();
     }
@@ -48,14 +70,17 @@ public class SystemMonitor extends AbstractMonitor implements MemoryMonitor, IOM
     }
     @Override
     public long getStart() {
+        assert state.ordinal() >= State.Started.ordinal();
         return ioMonitor.getStart();
     }
     @Override
     public long getFinish() {
+        assert state == State.Finished;
         return ioMonitor.getFinish();
     }
     @Override
     public long getDurationMillis() {
+        assert state == State.Finished;
         return ioMonitor.getDurationMillis();
     }
 }
