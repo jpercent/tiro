bc3be3a7e59e04ca3eee232d41424ad01a191c53
james <james@syndeticlogic.org>
2013-03-19 20:27:01 -0400

    Integrated Linux platform into the Jdbc

--- src/main/java/syndeticlogic/tiro/persistence/JdbcDao.java
+++ src/main/java/syndeticlogic/tiro/persistence/JdbcDao.java
@@ -11,7 +11,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
 import java.sql.PreparedStatement;
 
 public class JdbcDao {
@@ -181,12 +180,21 @@ public class JdbcDao {
     
     public void completeTrial(OSXAggregatedIOStats ioStats, OSXMemoryStats memoryStats, OSXCpuStats cpuStats, long duration, long trialId) {
         jdbcTemplate.update(completeTrial, duration, ioStats.getAverageMegabytesPerSecond(), cpuStats.getAverageUserModeTime(), 
-                cpuStats.getAverageSystemModeTime(), cpuStats.getAverageSystemModeTime(), memoryStats.getAverageFreePages(), 
+                cpuStats.getAverageSystemModeTime(), cpuStats.getAverageIdleModeTime(), memoryStats.getAverageFreePages(), 
                 memoryStats.getAverageActivePages(), memoryStats.getAverageInactivePages(), memoryStats.getAverageWiredPages(), 
                 memoryStats.getAverageNumberOfFaultRoutineCalls(), memoryStats.getAverageCopyOnWriteFaults(), memoryStats.getAverageZeroFilledPages(),
                 memoryStats.getAveragePageIns(), memoryStats.getAveragePageOuts(), trialId);
     }
-            
+
+	public void completeTrial(LinuxAggregatedIOStats aggregatedIOStats, LinuxMemoryStats memoryStats, LinuxCpuStats cpuStats, long duration, Long trialId) {
+        jdbcTemplate.update(completeTrial, duration, aggregatedIOStats.getAverageKpsRead(), cpuStats.getAverageUserModeTime(), 
+                cpuStats.getAverageSystemModeTime(), cpuStats.getAverageIowaitTime(), cpuStats.getAverageIdleModeTime(),                 
+                memoryStats.getFree(), memoryStats.getBuffers(), memoryStats.getCached(), memoryStats.getSwapCached(), memoryStats.getActive(), memoryStats.getActiveAnon(),
+            	memoryStats.getActiveFile(), memoryStats.getInactive(), memoryStats.getInactiveAnon(), memoryStats.getInactiveFile(), memoryStats.getUnevictable(),
+            	memoryStats.getSwapTotal(), memoryStats.getSwapFree(), memoryStats.getDirty(), memoryStats.getWriteback(), memoryStats.getAnon(), memoryStats.getSlab(),
+            	memoryStats.getSreclaim(), memoryStats.getSunreclaim(), memoryStats.getKernelStack(), memoryStats.getBounce(), memoryStats.getVmallocTotal(), 
+            	memoryStats.getVmallocUsed(), memoryStats.getVmallocChunk(), trialId);
+	}  
     public void insertController(Controller controller) { 
         synchronized(this) {
             controller.setId(controllersId++);
@@ -242,32 +250,6 @@ public class JdbcDao {
         });
     }
     
-    public void insertIOStats(OSXIOStats io, final long trialId) {
-        final List<Double> kilobytesPerTransfer = io.getRawKiloBytesPerTranferMeasurements();
-        final List<Double> transfersPerSecond = io.getRawTransfersPerSecond();
-        final List<Double> megabytesPerSecond = io.getRawMegabytesPerSecond();
-        final long id;
-        
-        synchronized(this) {
-            id = ioStatsId;
-            ioStatsId += megabytesPerSecond.size();
-        }
-        jdbcTemplate.batchUpdate(insertIOStats, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                ps.setLong(1, id+i);
-                ps.setLong(2, trialId);
-                ps.setDouble(3, kilobytesPerTransfer.get(i));
-                ps.setDouble(4, transfersPerSecond.get(i));
-                ps.setDouble(5, megabytesPerSecond.get(i));
-            }
-            @Override
-            public int getBatchSize() {
-                return megabytesPerSecond.size();
-            }
-        });
-    }
-
     public void insertMemoryStats(OSXMemoryStats monitor, final long trialId) {
         final List<Long> freePages = monitor.getRawFreePageMeasurements();
         final List<Long> activePages = monitor.getRawActivePageMeasurements();
@@ -307,6 +289,126 @@ public class JdbcDao {
         });
     }
 
+    public void insertMemoryStats(final LinuxMemoryStats stats, final Long trialId) {
+    	final List<Long> free = stats.getFree();
+    	final List<Long> buffers = stats.getBuffers();
+    	final List<Long> cached = stats.getCached();
+    	final List<Long>  swapcached = stats.getSwapCached();
+    	final List<Long>  active = stats.getActive();
+    	final List<Long>  activeAnon = stats.getActiveAnon();
+    	final List<Long>  activeFile = stats.getActiveFile();
+    	final List<Long>  inactive = stats.getInactive();
+    	final List<Long>  inactiveAnon= stats.getInactiveAnon();
+    	final List<Long>  inactiveFile = stats.getInactiveFile();
+    	final List<Long>  unevictable = stats.getUnevictable();
+    	final List<Long>  swapTotal = stats.getSwapTotal();
+    	final List<Long>  swapFree = stats.getSwapFree();
+    	final List<Long>  dirty = stats.getDirty();
+    	final List<Long>  writeback = stats.getWriteback();
+    	final List<Long>  anon = stats.getAnon();
+    	final List<Long>  slab = stats.getSlab();
+    	final List<Long>  sreclaim = stats.getSreclaim();
+    	final List<Long>  sunreclaim = stats.getSunreclaim();
+    	final List<Long>  kernelStack = stats.getKernelStack();
+    	final List<Long>  bounce = stats.getBounce();
+    	final List<Long>  vmallocTotal = stats.getVmallocTotal();
+    	final List<Long>  vmallocUsed = stats.getVmallocUsed();
+    	final List<Long>  vmallocChunk = stats.getVmallocChunk();
+    	final long id;
+        synchronized(this) {
+            id = memoryStatsId;
+            memoryStatsId += free.size();
+        }
+        jdbcTemplate.batchUpdate(insertMemoryStats, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+            	int j = 1;
+                ps.setLong(j++, id+i);
+                ps.setLong(j++, trialId);
+                ps.setLong(j++, free.get(i));
+                ps.setLong(j++, buffers.get(i));
+                ps.setLong(j++, cached.get(i));
+                ps.setLong(j++, swapcached.get(i));
+                ps.setLong(j++, active.get(i));
+                ps.setLong(j++, activeAnon.get(i));
+                ps.setLong(j++, activeFile.get(i));
+                ps.setLong(j++, inactive.get(i));
+                ps.setLong(j++, inactiveAnon.get(i));
+                ps.setLong(j++, inactiveFile.get(i));
+                ps.setLong(j++, unevictable.get(i));
+                ps.setLong(j++, swapTotal.get(i));
+                ps.setLong(j++, swapFree.get(i));
+                ps.setLong(j++, dirty.get(i));
+                ps.setLong(j++, writeback.get(i));
+                ps.setLong(j++, anon.get(i));
+                ps.setLong(j++, slab.get(i));
+                ps.setLong(j++, sreclaim.get(i));
+                ps.setLong(j++, sunreclaim.get(i));
+                ps.setLong(j++, kernelStack.get(i));
+                ps.setLong(j++, bounce.get(i));
+                ps.setLong(j++, vmallocTotal.get(i));
+                ps.setLong(j++, vmallocUsed.get(i));
+                ps.setLong(j++, vmallocChunk.get(i));
+            }
+            @Override
+            public int getBatchSize() {
+                return free.size();
+            }
+        });
+	}
+
+    public void insertIOStats(OSXIOStats io, final long trialId) {
+        final List<Double> kilobytesPerTransfer = io.getRawKiloBytesPerTranferMeasurements();
+        final List<Double> transfersPerSecond = io.getRawTransfersPerSecond();
+        final List<Double> megabytesPerSecond = io.getRawMegabytesPerSecond();
+        final long id;
+        
+        synchronized(this) {
+            id = ioStatsId;
+            ioStatsId += megabytesPerSecond.size();
+        }
+        jdbcTemplate.batchUpdate(insertIOStats, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setLong(1, id+i);
+                ps.setLong(2, trialId);
+                ps.setDouble(3, kilobytesPerTransfer.get(i));
+                ps.setDouble(4, transfersPerSecond.get(i));
+                ps.setDouble(5, megabytesPerSecond.get(i));
+            }
+            @Override
+            public int getBatchSize() {
+                return megabytesPerSecond.size();
+            }
+        });
+    }
+    
+	public void insertIOStats(final LinuxIOStats io, final Long trialId) {
+        final List<Double> tps = io.getRawTps();
+        final List<Double> kpsRead = io.getRawKpsRead();
+        final List<Double> kpsWritten = io.getRawKpsWritten();
+        final long id;
+        
+        synchronized(this) {
+            id = ioStatsId;
+            ioStatsId += tps.size();
+        }
+        jdbcTemplate.batchUpdate(insertIOStats, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setLong(1, id+i);
+                ps.setLong(2, trialId);
+                ps.setDouble(3, tps.get(i));
+                ps.setDouble(4, kpsRead.get(i));
+                ps.setDouble(5, kpsWritten.get(i));
+            }
+            @Override
+            public int getBatchSize() {
+                return tps.size();
+            }
+        });
+	}
+
     public void insertCpuStats(OSXCpuStats cpu, final long trialId) {
         final List<Long> userMode = cpu.getRawUserModeTime();
         final List<Long> systemMode = cpu.getRawSystemModeTime();
@@ -332,6 +434,36 @@ public class JdbcDao {
         });
     }
     
+	public void insertCpuStats(final LinuxCpuStats cpu, final Long trialId) {
+		final List<Double> userMode = cpu.getRawUserModeTime();
+		final List<Double> systemMode = cpu.getRawSystemModeTime();
+		final List<Double> iowait = cpu.getRawIowaitTime();
+		final List<Double> idleMode = cpu.getRawIdleModeTime();
+		final long id;
+		synchronized (this) {
+			id = cpuStatsId;
+			cpuStatsId += idleMode.size();
+		}
+		jdbcTemplate.batchUpdate(insertCpuStats,
+				new BatchPreparedStatementSetter() {
+					@Override
+					public void setValues(PreparedStatement ps, int i)
+							throws SQLException {
+						ps.setLong(1, id + i);
+						ps.setLong(2, trialId);
+						ps.setDouble(3, userMode.get(i));
+						ps.setDouble(4, systemMode.get(i));
+						ps.setDouble(4, iowait.get(i));
+						ps.setDouble(5, idleMode.get(i));
+					}
+
+					@Override
+					public int getBatchSize() {
+						return idleMode.size();
+					}
+				});
+	}
+	
     public List<?> adHocQuery(String sql, RowMapper<?> rowMapper, Map<String, Object> args) {
         return jdbcTemplate.query(sql, rowMapper, args);
     }
@@ -339,16 +471,4 @@ public class JdbcDao {
     public List<?> adHocQuery(String sql, RowMapper<?> rowMapper) {
         return jdbcTemplate.query(sql, rowMapper);
     }
-
-	public void insertIOStats(IOStats iostat, Long trialId) {
-	}
-
-	public void insertMemoryStats(LinuxMemoryStats memoryStats, Long trialId) {
-	}
-
-	public void insertCpuStats(LinuxCpuStats cpuStats, Long trialId) {
-	}
-
-	public void completeTrial(LinuxAggregatedIOStats aggregatedIOStats, LinuxMemoryStats memoryStats, LinuxCpuStats cpuStats, long duration, Long trialId) {
-	}
 }
